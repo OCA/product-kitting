8ac98d2aaa51731b65a02b2247e7722466f80346
unknown <nicolas.bessi@camptocamp.com>
2012-04-24 10:53:45 +0200

    [ADD] moved modules (lp:c2c-addons/6.1  rev 28.1.10)

---
+++ bom_stock/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Compute Stock from BoM',
+    'version': '5.0',
+    'category': 'Generic Modules/Others',
+    'description':
+     """Compute the BOM  stock Value. BoM Stock Value are computed by:
+      "(Reference stock" of "Product" + How much could I produce of That "Product" according to the component's "Reference Stock)"
+      "This reference stock can be chosen by company through a selection field and can be one of the available stock quantity computed in the system : Available stock, Virtual stock, immediately_usable stock (from stock_available_immediately)".""" 
+    ,
+    'author': 'Camptocamp',
+    'website': 'http://www.camptocamp.com',
+    'depends': ['stock', 'stock_available_immediately'],
+    'init_xml': [],
+    'update_xml': ['bom_stock_view.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ bom_stock/bom_stock.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Joël Grand-Guillaume, Guewen Baconnier
+#    Copyright 2010-2012 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import decimal_precision as dp
+
+from osv import fields, osv
+
+
+class res_company(osv.osv):
+
+    _inherit = 'res.company'
+
+    _columns = {
+        'ref_stock': fields.selection(
+            [('real', 'Real Stock'),
+             ('virtual', 'Virtual Stock'),
+             ('immediately', 'Immediately Usable Stock')],
+            'Reference Stock for BoM Stock')
+    }
+
+    _defaults = {
+        'ref_stock': lambda *a: 'real'
+    }
+
+res_company()
+
+
+class product_bom_stock_value(osv.osv):
+    """
+    Inherit Product in order to add an "Bom Stock" field
+    """
+
+    def _bom_stock_mapping(self, cr, uid, context=None):
+        return {'real': 'qty_available',
+                'virtual': 'virtual_available',
+                'immediately': 'immediately_usable_qty'}
+
+    def _compute_bom_stock(self, cr, uid, product,
+                           quantities, company, context=None):
+        bom_obj = self.pool.get('mrp.bom')
+
+        mapping = self._bom_stock_mapping(cr, uid, context=context)
+        stock_field = mapping[company.ref_stock]
+
+        product_qty = quantities.get(stock_field, 0.0)
+        # find a bom on the product
+        bom_id = bom_obj._bom_find(
+            cr, uid, product.id, product.uom_id.id, properties=[])
+
+        if bom_id:
+            prod_min_quantities = []
+            bom = bom_obj.browse(cr, uid, bom_id, context=context)
+            if bom.bom_lines:
+                stop_compute_bom = False
+                # Compute stock qty of each product used in the BoM and
+                # get the minimal number of items we can produce with them
+                for line in bom.bom_lines:
+                    prod_min_quantity = 0.0
+                    bom_qty = line.product_id[stock_field]
+
+                    # the reference stock of the component must be greater
+                    # than the quantity of components required to
+                    # build the bom
+                    if bom_qty >= line.product_qty:
+                        prod_min_quantity = (
+                            (bom_qty *
+                             line.product_id.uom_id.factor /
+                             line.product_uom.factor) /
+                            line.product_qty)  # line.product_qty is always > 0
+                    else:
+                        # if one product has not enough stock,
+                        # we do not need to compute next lines
+                        # because the final quantity will be 0.0 in any case
+                        stop_compute_bom = True
+
+                    prod_min_quantities.append(prod_min_quantity)
+
+                    if stop_compute_bom:
+                        break
+
+            product_qty += min(prod_min_quantities)
+        return product_qty
+
+    def _product_available(self, cr, uid, ids, field_names=None,
+                           arg=False, context=None):
+        # We need available, virtual or immediately usable
+        # quantity which is selected from company to compute Bom stock Value
+        # so we add them in the calculation.
+        user_obj = self.pool.get('res.users')
+        comp_obj = self.pool.get('res.company')
+        if 'bom_stock' in field_names:
+            field_names.append('qty_available')
+            field_names.append('immediately_usable_qty')
+            field_names.append('virtual_available')
+
+        res = super(product_bom_stock_value, self)._product_available(
+            cr, uid, ids, field_names, arg, context)
+
+        if 'bom_stock' in field_names:
+            company = user_obj.browse(cr, uid, uid, context=context).company_id
+            if not company:
+                company_id = comp_obj.search(cr, uid, [], context=context)[0]
+                company = comp_obj.browse(cr, uid, company_id, context=context)
+
+            for product_id, stock_qty in res.iteritems():
+                product = self.browse(cr, uid, product_id, context=context)
+                res[product_id]['bom_stock'] = \
+                    self._compute_bom_stock(
+                        cr, uid, product, stock_qty, company, context=context)
+        return res
+
+    _inherit = 'product.product'
+
+    _columns = {
+        'qty_available': fields.function(
+            _product_available,
+            multi='qty_available',
+            type='float',
+            digits_compute=dp.get_precision('Product UoM'),
+            string='Quantity On Hand',
+            help="Current quantity of products.\n"
+                 "In a context with a single Stock Location, this includes "
+                 "goods stored at this Location, or any of its children.\n"
+                 "In a context with a single Warehouse, this includes "
+                 "goods stored in the Stock Location of this Warehouse, "
+                 "or any "
+                 "of its children.\n"
+                 "In a context with a single Shop, this includes goods "
+                 "stored in the Stock Location of the Warehouse of this Shop, "
+                 "or any of its children.\n"
+                 "Otherwise, this includes goods stored in any Stock Location "
+                 "typed as 'internal'."),
+        'virtual_available': fields.function(
+            _product_available,
+            multi='qty_available',
+            type='float',
+            digits_compute=dp.get_precision('Product UoM'),
+            string='Quantity Available',
+            help="Forecast quantity (computed as Quantity On Hand "
+                 "- Outgoing + Incoming)\n"
+                 "In a context with a single Stock Location, this includes "
+                 "goods stored at this Location, or any of its children.\n"
+                 "In a context with a single Warehouse, this includes "
+                 "goods stored in the Stock Location of this Warehouse, "
+                 "or any "
+                 "of its children.\n"
+                 "In a context with a single Shop, this includes goods "
+                 "stored in the Stock Location of the Warehouse of this Shop, "
+                 "or any of its children.\n"
+                 "Otherwise, this includes goods stored in any Stock Location "
+                 "typed as 'internal'."),
+        'incoming_qty': fields.function(
+            _product_available,
+            multi='qty_available',
+            type='float',
+            digits_compute=dp.get_precision('Product UoM'),
+            string='Incoming',
+            help="Quantity of products that are planned to arrive.\n"
+                 "In a context with a single Stock Location, this includes "
+                 "goods arriving to this Location, or any of its children.\n"
+                 "In a context with a single Warehouse, this includes "
+                 "goods arriving to the Stock Location of this Warehouse, or "
+                 "any of its children.\n"
+                 "In a context with a single Shop, this includes goods "
+                 "arriving to the Stock Location of the Warehouse of this "
+                 "Shop, or any of its children.\n"
+                 "Otherwise, this includes goods arriving to any Stock "
+                 "Location typed as 'internal'."),
+        'outgoing_qty': fields.function(
+            _product_available,
+            multi='qty_available',
+            type='float',
+            digits_compute=dp.get_precision('Product UoM'),
+            string='Outgoing',
+            help="Quantity of products that are planned to leave.\n"
+                 "In a context with a single Stock Location, this includes "
+                 "goods leaving from this Location, or any of its children.\n"
+                 "In a context with a single Warehouse, this includes "
+                 "goods leaving from the Stock Location of this Warehouse, or "
+                 "any of its children.\n"
+                 "In a context with a single Shop, this includes goods "
+                 "leaving from the Stock Location of the Warehouse of this "
+                 "Shop, or any of its children.\n"
+                 "Otherwise, this includes goods leaving from any Stock "
+                 "Location typed as 'internal'."),
+        'immediately_usable_qty': fields.function(
+            _product_available,
+            digits_compute=dp.get_precision('Product UoM'),
+            type='float',
+            string='Immediately Usable',
+            multi='qty_available',
+            help="Quantity of products really available for sale." \
+                 "Computed as: Quantity On Hand - Outgoing."),
+        'bom_stock': fields.function(
+            _product_available,
+            digits_compute=dp.get_precision('Product UoM'),
+            type='float',
+            string='Bill of Materials Stock',
+            help="Quantities of products based on Bill of Materials, "
+                 "useful to know how much of this "
+                 "product you could produce. "
+                 "Computed as:\n "
+                 "Reference stock of this product + "
+                 "how much could I produce of this product with the BoM"
+                 "Components",
+            multi='qty_available'),
+    }
+
+product_bom_stock_value()

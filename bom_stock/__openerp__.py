ba52b87251d29c86dc21d3ae99a1b610f79da914
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-03-02 17:26:41 +0100

    Add OCA as author of OCA addons
    In order to get visibility on https://www.odoo.com/apps the OCA board has
    decided to add the OCA as author of all the addons maintained as part of the
    association.

--- bom_stock/__openerp__.py
+++ bom_stock/__openerp__.py
@@ -31,7 +31,7 @@
       Available stock, Virtual stock, immediately_usable stock (from
       stock_available_immediately)."""
     ,
-    'author': 'Camptocamp',
+    'author': "Camptocamp,Odoo Community Association (OCA)",
     'website': 'http://www.camptocamp.com',
     'depends': ['stock',
                 'mrp',
